5454059841dc098786a3f30398e10b3fb6b257e9
bobzoot2007 <izuaby@gmail.com>
2021-06-29 23:46:09 +0300

    Stuff added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+
+interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Oliver Ray",
+      url: "https://pps.whatsapp.net/v/t61.24694-24/205663138_1139058829905645_8454034704936361956_n.jpg?ccb=11-4&oh=72a1cc0ddae2a6943d87234d7eea75c0&oe=60DFEEFD",
+      age: 14,
+      note: "Old school friend of mine",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to the party</h1>
+      <List people={people} />
     </div>
   );
 }
